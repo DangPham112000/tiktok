d54a55764b68e9b2a8370319da28bd58374147f6
DangPham <dangpham112000@users.noreply.github.com>
2021-10-13 23:09:09 +0700

    useState - initialState(callback) - sum orders and counter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 
+const orders = [300, 500, 700];
+
 function App() {
-  const [counter, setCounter] = useState(1);
+  // const total = orders.reduce((total, order) => {
+  //   return total + order;
+  // });
+  // console.log(total);
+  
+  const [counter, setCounter] = useState(() => {
+    
+    const total = orders.reduce((totalOrders, order) => totalOrders + order);
+    
+    console.log(total);
+    return total;
+  });
   
   const increaseCounter = () => {
-    setCounter(prevCounter => prevCounter + 1);
-    setCounter(prevCounter => prevCounter + 1);
     setCounter(prevCounter => prevCounter + 1);
   }
   
   return (
     <div className="App" style={{padding: "12px"}}>
       <h1>{counter}</h1>
-      <button onClick={increaseCounter}>plus + 3</button>
+      <button onClick={increaseCounter}>plus</button>
     </div>
   );
 }
