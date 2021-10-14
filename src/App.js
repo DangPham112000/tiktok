cdb643f10e653a9c05424da29290028c2b5951eb
DangPham <dangpham112000@users.noreply.github.com>
2021-10-14 10:53:48 +0700

    two-way binding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import { useState } from 'react';
 
-const gifts = [
-  'CPU i9',
-  'RAM 32GB',
-  'SSD 1TB',
-  'GPU Nvidia 1060ti'
-];
-
 function App() {
-  const [gift, setGift] = useState();
+  const [name, setName] = useState('');
 
-  const randomGift = () => {
-    const randomIndex = Math.floor(Math.random() * 4);
-    // console.log(randomIndex);
-
-    setGift(gifts[randomIndex]);
+  const getName = (e) => {
+    setName(e.target.value);
+  };
+  
+  const resetName = (e) => {
+    setName('Pham Hai Dang')
   };
 
+  console.log(name);
+  
   return (
     <div className="App" style={{padding: "60px"}}>
-      <h1>{gift || 'Chưa có phần thưởng'}</h1>
-      <button onClick={randomGift}>GACHA</button>
+      <input value={name} onChange={getName}/>
+      <button onClick={resetName}>Reset</button>
     </div>
   );
 }
