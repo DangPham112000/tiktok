67902c47e1812d32219ed59010a05c0a268d8dad
DangPham <dangpham112000@users.noreply.github.com>
2021-10-14 11:17:15 +0700

    useState 2-way binding - ratio form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import { useState } from 'react';
 
+// Response from API
+const courses = [
+  {
+    id: 1,
+    name: 'HTML/CSS'
+  },
+  {
+    id: 2,
+    name: 'JavaScript'
+  },
+  {
+    id: 3,
+    name: 'Python'
+  },
+  {
+    id: 4,
+    name: 'ReactJS'
+  }
+];
+
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [idChecked, setIdChecked] = useState(2);
   
   const submit = () => {
     // Call API
     console.log({
-      name, 
-      email
+      idChecked
     });
   };
   
   return (
     <div className="App" style={{padding: "60px"}}>
-      <input value={name} onChange={(e) => { setName(e.target.value) }}/>
-      <br />
-      <input value={email} onChange={(e) => { setEmail(e.target.value) }}/>
+      {
+        courses.map(course => {
+          return (
+            <div key={course.id} >
+              <input 
+                type="radio"
+                checked={idChecked === course.id}
+                onChange={() => setIdChecked(course.id)} 
+              />
+              {course.name}
+            </div>
+          );
+        })
+      }
       <button onClick={submit}>submit</button>
     </div>
   );
