16c8d72ed83b77d9122e032afd532d262fb193dd
DangPham <dangpham112000@users.noreply.github.com>
2021-10-14 10:01:03 +0700

    useState - random gifts app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { useState } from 'react';
 
+const gifts = [
+  'CPU i9',
+  'RAM 32GB',
+  'SSD 1TB',
+  'GPU Nvidia 1060ti'
+];
 
 function App() {
-  const [info, setInfo] = useState({
-    name: 'Pham Hai Dang',
-    age: 21,
-    job: 'student'
-  });
+  const [gift, setGift] = useState();
 
-  const updateInfo = () => {
-    setInfo({
-      ...info,
-      bio: 'dep trai con lai 12 dong mau'
-    });
+  const randomGift = () => {
+    const randomIndex = Math.floor(Math.random() * 4);
+    // console.log(randomIndex);
+
+    setGift(gifts[randomIndex]);
   };
 
   return (
-    <div className="App" style={{padding: "12px"}}>
-      <h1>{JSON.stringify(info)}</h1>
-      <button onClick={updateInfo}>update info</button>
+    <div className="App" style={{padding: "60px"}}>
+      <h1>{gift || 'Chưa có phần thưởng'}</h1>
+      <button onClick={randomGift}>GACHA</button>
     </div>
   );
 }
