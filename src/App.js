c9666bf677857b5578488ebd326f819e3ff61984
DangPham <dangpham112000@users.noreply.github.com>
2021-10-16 20:34:22 +0700

    Mounted/Unmounted

--- src/App.js
+++ src/App.js
@@ -1,64 +1,13 @@
 import { useState } from 'react';
-
-// Response from API
-const courses = [
-  {
-    id: 1,
-    name: 'HTML/CSS'
-  },
-  {
-    id: 2,
-    name: 'JavaScript'
-  },
-  {
-    id: 3,
-    name: 'Python'
-  },
-  {
-    id: 4,
-    name: 'ReactJS'
-  }
-];
+import Content from './Content';
 
 function App() {
-  const [idsChecked, setIdsChecked] = useState([]);
-  
-  const handleChecked = (id) => {
-    setIdsChecked(prevIds => {
-      if (idsChecked.includes(id)) {
-        return idsChecked.filter(idChecked => idChecked !== id);
-      } else {
-        return [...idsChecked, id];
-      }
-    });
-  };
+  const [show, setShow] = useState(false);
 
-  console.log(idsChecked);
-  
-  const submit = () => {
-    // Call API
-    console.log({
-      ids: idsChecked
-    });
-  };
-  
   return (
     <div className="App" style={{padding: "60px"}}>
-      {
-        courses.map(course => {
-          return (
-            <div key={course.id} >
-              <input 
-                type="checkbox"
-                checked={idsChecked.includes(course.id)}
-                onChange={() => { handleChecked(course.id); }} 
-              />
-              {course.name}
-            </div>
-          );
-        })
-      }
-      <button onClick={submit}>submit</button>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content />}
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,9 @@
+
+
+function Content() {
+    return (
+        <h1>Mounted / Unmounted</h1>
+    );
+}
+
+export default Content;
