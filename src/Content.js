9d8b4092bdcf29886f6934950ffe4ca24d15d382
DangPham <dangpham112000@users.noreply.github.com>
2021-11-22 10:35:25 +0700

    useRef - take timerId in this func to that func - countdown auto

--- src/Content.js
+++ src/Content.js
@@ -1,42 +1,27 @@
-import { useState, useEffect, useLayoutEffect } from 'react';
-
-// useEffect
-// 1. Cập nhật lại state
-// 2. Cập nhật DOM (mutated)
-// 3. Render lại UI
-// 4. Gọi cleanup nếu deps thay đổi
-// 5. Gọi useEffect callback 
-
-// useLayoutEffect
-// 1. Cập nhật lại state
-// 2. Cập nhật DOM (mutated)
-// 3. Gọi cleanup nếu deps thay đổi (sync)
-// 4. Gọi useEffect callback (sync)
-// 5. Render lại UI
+import { useState, useRef } from 'react';
 
 function Content() {
-    const [number, setNumber] = useState(0);
+    const [number, setNumber] = useState(80);
 
-    // render 0 1 2 3 4 0 1 2....
-    // useEffect(() => {
-    //     if (number > 3) 
-    //         setNumber(0);
-    // }, [number]);
+    const timerId = useRef();
 
-    // render 0 1 2 3 0 1 2....
-    useLayoutEffect(() => {
-        if (number > 3) 
-            setNumber(0);
-    }, [number]);
-
-    const handleCount = () => {
-        setNumber(number + 1);
+    const handleRun = () => {
+        timerId.current = setInterval(() => {
+            setNumber(prevNumber => prevNumber - 1);
+        }, 100);
+        console.log('Run: ', timerId);
     }
     
+    const handleStop = () => {
+        clearInterval(timerId.current);
+        console.log('Stop: ', timerId);
+    }
+
     return (
         <div>
             <h2>{number}</h2>
-            <button onClick={handleCount}>Lets+1</button>
+            <button onClick={handleRun}>Lets go</button>
+            <button onClick={handleStop}>Stop it</button>
         </div>
     );
 }
