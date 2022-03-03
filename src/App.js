66fec3a634908bc7de2d98cf4b437c2eefda4973
DangPham <dangpham112000@users.noreply.github.com>
2022-03-04 03:12:32 +0700

    React-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import Button from "./Button";
+import { Link, Route, Routes } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Reviews from "./pages/Reviews";
 
 
 function App() {
   
-  
   return (
     <div className="App" style={{ padding: "60px" }}>
-      <Button primary/>
+      <nav>
+        <span><Link to="/">Home page</Link></span>
+        <span><Link to="/contact">Contact page</Link></span>
+        <span><Link to="/reviews">Reviews page</Link></span>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/reviews" element={<Reviews/>} />
+      </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GlobalStyle from './GlobalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle>
+    <BrowserRouter>
       <App />
-    </GlobalStyle>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
